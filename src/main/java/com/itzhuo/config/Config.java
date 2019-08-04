8c7d8bf6f46250b081e591f68d34ab3c67610dff
demigod <zhuoshengqi@163.com>
2019-08-04 15:16:04 +0800

    增加myBaties plus 分布查询的配置类

---
+++ src/main/java/com/itzhuo/config/Config.java
@@ -0,0 +1,14 @@
+package com.itzhuo.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}
